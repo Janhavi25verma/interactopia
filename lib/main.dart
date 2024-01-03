81aec939061e05714b065f1aeae0b38c8f9959df
Janhavi25verma <janhavi25verma@gmail.com>
2024-01-04 03:42:47 +0530

    Firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:interactopia/features/auth/screens/login_screen.dart';
 import 'package:interactopia/theme/palette.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
